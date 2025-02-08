a8226c6252ac2d4189ed4d408af252f3f2582ad1
Shubham Vishwakarma <vi453770@gmail.com>
2025-02-09 01:09:34 +0530

    our todo using react is complited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,117 @@
 import React, { useEffect, useState } from "react";
-import { uuidv4 as newId } from "uuid";
+import { v4 as newId } from "uuid";
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
   const [showFinished, setShowFinished] = useState(false);
 
   useEffect(() => {
-    const todoString = localStorage.getItem("todos");
-    if (todoString) {
-      const fetchTodos = JSON.parse(localStorage.getItem("todos"));
-      setTodos(fetchTodos);
+    const checkTodoString = localStorage.getItem("todos");
+    if (checkTodoString) {
+      const todoString = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoString);
     }
-  });
-  const updateTodoList = (updatedTodo) => {
-    localStorage.setItem("todos", JSON.stringify(updatedTodo));
-  };
+  }, []);
 
-  const updateToggle = (params) => {
-    setShowFinished(!showFinished);
-  };
-
-  const addTodo = () => {
-    const newTodo = [...todos, { id: newId(), todo, isComplited: false }];
-    setTodos(newTodo);
-    updateTodoList(newTodo);
-    setTodo("");
+  const todoList = (upadted) => {
+    localStorage.setItem("todos", JSON.stringify(upadted));
   };
 
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
+  const handleRemove = (e, id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+    todoList(newTodos);
+  };
 
-  return <div></div>;
+  const handleAdd = () => {
+    const newTodo = [...todos, { id: newId(), todo, isComplited: false }];
+    setTodos(newTodo);
+    todoList(newTodo);
+    setTodo("");
+  };
+
+  const handleEdit = (e, id) => {
+    const todoFinds = todos.filter((todo) => {
+      return todo.id === id;
+    });
+
+    setTodo(todoFinds[0].todo);
+
+    const newTodos = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    setTodos(newTodos);
+    todoList(newTodos);
+  };
+
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex((item) => {
+      return item.id == id;
+    });
+    let newTodos = [...todos];
+    newTodos[index].isComplited = !newTodos[index].isComplited;
+    setTodos(newTodos);
+    todoList(newTodos);
+  };
+  const togleFinish = () => {
+    setShowFinished(() => !showFinished);
+  };
+  return (
+    <div>
+      <div>
+        <h1>write your task here</h1>
+        <input
+          type="text"
+          placeholder="write todo"
+          value={todo}
+          onChange={handleChange}
+        />
+        <button onClick={handleAdd}>Save</button>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          id="show"
+          onChange={togleFinish}
+          checked={showFinished}
+        />
+        <label htmlFor="show">show finished</label>
+      </div>
+      <div>
+        {todos.length === 0 && <div>No Todos to display</div>}
+        {todos.map((item) => {
+          return (
+            (showFinished || !item.isComplited) && (
+              <div key={item.id}>
+                <input
+                  onChange={handleCheckbox}
+                  type="checkbox"
+                  checked={item.isComplited}
+                  name={item.id}
+                />
+                <label
+                  htmlFor=""
+                  style={{
+                    textDecoration: item.isComplited ? "line-through" : "",
+                  }}
+                >
+                  {item.todo}
+                </label>
+                <button onClick={(e) => handleEdit(e, item.id)}>Edit</button>
+                <button onClick={(e) => handleRemove(e, item.id)}>
+                  remove
+                </button>
+              </div>
+            )
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default App;
